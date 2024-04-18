b2ec9663d21a0965da4ccee75c6ae491174f380c
Wesley Praxedes <wesleyprofile@gmail.com>
2024-04-18 15:34:30 -0300

    Implement in database security

--- springprojectmich2/src/main/java/com/parkingcontrol/security/WebSecurityConfig.java
+++ springprojectmich2/src/main/java/com/parkingcontrol/security/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.parkingcontrol.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -10,6 +11,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    UserDetailsServiceImpl
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
